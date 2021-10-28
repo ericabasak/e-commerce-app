8fcf45e14ff7b67edd360e135fb95ee91e31bd3d
Erica Basak <ericabasak@gmail.com>
2021-10-28 10:03:58 -0400

    migrations done and schema ready

---
+++ db/migrate/20211028103104_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.decimal :sub_total
+
+      t.timestamps
+    end
+  end
+end
